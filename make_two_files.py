86cd8eaf69e130ff6f32f1a8aeec83d2614eacf7
SeoMar <118463057+seomar2022@users.noreply.github.com>
2024-07-25 20:02:42 +0900

    feat: Run Excel

--- make_two_files.py
+++ make_two_files.py
@@ -3,7 +3,7 @@ import pyautogui
 import sys
 import pandas as pd
 import webbrowser
-import shutil #csv파일 복사하기위해
+import win32com.client
 
 ####엑셀 파일 읽어오기
 # 배송리스트가 담긴 폴더 읽어오기
@@ -20,6 +20,7 @@ else:
     pyautogui.alert(f"{delivery_list_folder_name} 폴더에 파일이 없습니다!", button="프로그램 종료")
     sys.exit() #프로그램 종료
 
+
 ####카페24 양식에 맞게 수정
 try:
     # C열의 데이터까지만 남겨두기.
@@ -33,6 +34,25 @@ try:
  
 except Exception as e:
     print(f"파일 편집 중 오류가 발생했습니다: {e}")
+
+####매크로 실행
+#프린터 지정
+#매크로 실행
+try:
+    # 엑셀 애플리케이션 시작
+    excel = win32com.client.Dispatch("Excel.Application")
+    
+    # 엑셀 애플리케이션을 표시하지 않음 (백그라운드에서 실행)
+    excel.Visible = False
+    
+    
+    # 엑셀 애플리케이션 종료
+    excel.Quit()
+    
+  #  print(f"'{macro_name}' 매크로가 성공적으로 실행되었습니다.")
+    
+except Exception as e:
+    print(f"실행 중 오류가 발생했습니다: {e}")
 
 
 ####한진택배 사이트 열기
